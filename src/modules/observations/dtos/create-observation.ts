ad4d1d837468420262b29eb2fc286862969bb940
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-12-06 17:28:25 -0400

    feat: finished observations module partially

--- src/modules/observations/dtos/create-observation.ts
+++
@@ -1,8 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { Observation } from '../entities/observations.entity';
-
-export class CreateObservationDto extends OmitType(Observation, [
-  'id',
-  'created_at',
-  'updated_at',
-]) {}

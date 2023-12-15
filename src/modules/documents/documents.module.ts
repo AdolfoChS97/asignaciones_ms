0ad7c783c35f70af3d565f3e38b458b8404105a3
Adolfo Andres Chafardett Silva <36141497+AdolfoChS97@users.noreply.github.com>
2023-12-15 16:18:53 -0400

    Revert "Feat/template"

--- src/modules/documents/documents.module.ts
+++ src/modules/documents/documents.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentsService } from './documents.service';
 import { DocumentsController } from './documents.controller';
 import { Document } from './entities/document.entity';
-import { SharedModule } from '@/shared/modules/shared.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Document]), SharedModule],
+  imports: [TypeOrmModule.forFeature([Document])],
   controllers: [DocumentsController],
   providers: [DocumentsService],
 })
--- src/modules/documents/dto/create-document.dto.ts
+++ src/modules/documents/dto/create-document.dto.ts
@@ -1,30 +1,6 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { Document } from '../entities/document.entity';
 import { HttpStatus } from '@nestjs/common';
-import { IsNotEmpty } from 'class-validator';
-
-export class CrearPuntosDeCuentaDto extends OmitType(Document, [
-  'id',
-  'base64',
-  'created_at',
-  'updated_at',
-]) {
-  @ApiProperty({
-    type: 'string',
-    example: 'plantilla',
-    description: 'Nombre de la plantilla del punto de cuenta',
-  })
-  @IsNotEmpty()
-  fileName: string;
-
-  @ApiProperty({
-    type: 'object',
-    example: { mensaje: 'Hola mundo' },
-    description: 'Propiedades del punto de cuenta',
-  })
-  @IsNotEmpty()
-  data: [key: string, value: string];
-}
 
 export class CreateDocumentDto extends OmitType(Document, [
   'id',

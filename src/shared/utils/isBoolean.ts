c4815b2ffeea7d4c6102661fa1d361adb8123d7e
Josue ortiz <ortizjd0036@gmail.com>
2023-12-17 10:34:16 -0400

    fix query filter get all param

--- src/shared/utils/isBoolean.ts
+++ src/shared/utils/isBoolean.ts
@@ -9,12 +9,3 @@ export function isBoolean(valor) {
   // Si el valor no es un booleano, lanzamos un error
   throw new BadRequestException('El valor no es booleano');
 }
-
-
-
-export function optionBoolean(statusParam: any){
-  if (typeof statusParam === 'boolean') return true;
-  if (typeof statusParam === 'string' && (statusParam === 'true' || statusParam === 'false')) return true;
-  return false;
-}
-

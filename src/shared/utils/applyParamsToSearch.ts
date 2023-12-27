cd5c2f9d280da18629f7f3fb2ea35fac42a514f5
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-12-26 21:19:45 -0400

    fix: It was modified the util function applyParamsToSearch

--- src/shared/utils/applyParamsToSearch.ts
+++ src/shared/utils/applyParamsToSearch.ts
@@ -1,8 +1,14 @@
+import { Like } from 'typeorm';
+
 export function applyParamsToSearch(queryParams, target) {
   if (Object.keys(queryParams).length > 0) {
     Object.keys(queryParams).forEach((key) => {
-      if (!isNaN(+queryParams[key])) {
-        target['where'][key] = +queryParams[key];
+      const value = queryParams[key];
+
+      if (typeof value === 'string') {
+        target['where'][key] = Like(`%${value}%`);
+      } else if (!isNaN(+value)) {
+        target['where'][key] = +value;
       }
     });
   }
